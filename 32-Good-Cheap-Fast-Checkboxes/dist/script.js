a9fa6c014d826c740cf710e335cc9c44107e00e7
Leandro Grisalena Morello <leagrisa@WKMZT0DD141F.global.publicisgroupe.net>
2021-04-23 16:27:51 -0300

    32-Good-Cheap-Fast-Checkboxes - final

--- 32-Good-Cheap-Fast-Checkboxes/dist/script.js
+++ 32-Good-Cheap-Fast-Checkboxes/dist/script.js
@@ -16,72 +16,18 @@ var select = function select(el) {
     selectAll = function selectAll(el) {
   return [].slice.call(document.querySelectorAll(el));
 },
-    resultEl = select('#result'),
-    lengthEl = select('#length'),
-    upperCaseEl = select('#uppercase'),
-    lowerCaseEl = select('#lowercase'),
-    numbersEl = select('#numbers'),
-    symbolsEl = select('#symbols'),
-    generateEl = select('#generate'),
-    clipboardEl = select('#clipboard'),
-    getRandom = function getRandom(num, num2) {
-  return String.fromCharCode(Math.floor(Math.random() * num) + num2);
-},
-    //26,10, 97,65, 48
-getRandomSymbol = function getRandomSymbol() {
-  var symbols = '!@#$%^&*(){}[]=<>/,.';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-},
-    randomFunc = {
-  lower: getRandom,
-  upper: getRandom,
-  number: getRandom,
-  symbol: getRandomSymbol
-},
-    generatePassword = function generatePassword(lower, upper, number, symbol, length) {
-  var generatedPassword = "";
-  var typesCount = lower + upper + number + symbol,
-      typesArr = [{
-    lower: lower
-  }, {
-    upper: upper
-  }, {
-    number: number
-  }, {
-    symbol: symbol
-  }].filter(function (item) {
-    return Object.values(item)[0];
-  });
-  if (typesCount === 0) return "";
-
-  for (var i = 0; i < length; i += typesCount) {
-    typesArr.forEach(function (type) {
-      var funcName = Object.keys(type)[0],
-          value = funcName === "lower" ? [26, 97] : funcName === "upper" ? [26, 65] : funcName === "number" ? [10, 48] : [];
-      generatedPassword += randomFunc[funcName].apply(randomFunc, value);
-    });
+    toggles = selectAll(".toggle"),
+    good = select("#good"),
+    cheap = select("#cheap"),
+    fast = select("#fast"),
+    doTheTrick = function doTheTrick(e) {
+  if (good.checked && cheap.checked && fast.checked) {
+    good === e.target && (fast.checked = false);
+    cheap === e.target && (good.checked = false);
+    fast === e.target && (cheap.checked = false);
   }
-
-  var finalPassword = generatedPassword.slice(0, length);
-  return finalPassword;
 };
 
-clipboardEl.addEventListener("click", function () {
-  var textArea = document.createElement("textarea"),
-      password = resultEl.textContent;
-  if (!password) return;
-  textArea.value = password;
-  document.body.appendChild(textArea);
-  textArea.select();
-  document.execCommand("copy");
-  document.body.removeChild(textArea);
-  alert("Password copied to clipboard!!");
-});
-generateEl.addEventListener("click", function () {
-  var length = +lengthEl.value;
-  var hasLower = lowerCaseEl.checked,
-      hasUpper = upperCaseEl.checked,
-      hasNumber = numbersEl.checked,
-      hasSymbol = symbolsEl.checked;
-  resultEl.textContent = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+toggles.forEach(function (toggle) {
+  return toggle.addEventListener("change", doTheTrick);
 });
--- 32-Good-Cheap-Fast-Checkboxes/src/script.js
+++ 32-Good-Cheap-Fast-Checkboxes/src/script.js
@@ -12,72 +12,16 @@
 
  const select = el => (document.querySelector(el)),
      selectAll = el => [].slice.call(document.querySelectorAll(el)),
-     resultEl = select('#result'),
-     lengthEl = select('#length'),
-     upperCaseEl = select('#uppercase'),
-     lowerCaseEl = select('#lowercase'),
-     numbersEl = select('#numbers'),
-     symbolsEl = select('#symbols'),
-     generateEl = select('#generate'),
-     clipboardEl = select('#clipboard'),
-
-     getRandom = (num, num2) => String.fromCharCode(Math.floor(Math.random() * num) + num2), //26,10, 97,65, 48
-     getRandomSymbol = () => {
-         const symbols = '!@#$%^&*(){}[]=<>/,.';
-         return symbols[Math.floor(Math.random() * symbols.length)]
-     },
-     randomFunc = {
-         lower: getRandom,
-         upper: getRandom,
-         number: getRandom,
-         symbol: getRandomSymbol
-     },
-     generatePassword = (lower, upper, number, symbol, length) => {
-         let generatedPassword = "";
-         const typesCount = lower + upper + number + symbol,
-             typesArr = [{
-                 lower
-             }, {
-                 upper
-             }, {
-                 number
-             }, {
-                 symbol
-             }].filter(item => Object.values(item)[0]);
-
-         if (typesCount === 0) return "";
-
-         for (let i = 0; i < length; i += typesCount) {
-             typesArr.forEach(type => {
-                 const funcName = Object.keys(type)[0],
-                     value = funcName === "lower" ? [26, 97] : funcName === "upper" ? [26, 65] : funcName === "number" ? [10, 48] : [];
-                 generatedPassword += randomFunc[funcName](...value);
-             })
+     toggles = selectAll(".toggle"),
+     good = select("#good"),
+     cheap = select("#cheap"),
+     fast = select("#fast"),
+     doTheTrick = (e) => {
+         if (good.checked && cheap.checked && fast.checked) {
+             good === e.target && (fast.checked = false);
+             cheap === e.target && (good.checked = false);
+             fast === e.target && (cheap.checked = false);
          }
-         const finalPassword = generatedPassword.slice(0, length);
+     }
 
-         return finalPassword;
-     };
-
- clipboardEl.addEventListener("click", () => {
-     const textArea = document.createElement("textarea"),
-         password = resultEl.textContent;
-     if (!password) return;
-     textArea.value = password;
-     document.body.appendChild(textArea);
-     textArea.select();
-     document.execCommand("copy");
-     document.body.removeChild(textArea);
-     alert("Password copied to clipboard!!");
- })
-
-
- generateEl.addEventListener("click", () => {
-     const length = +lengthEl.value;
-     const hasLower = lowerCaseEl.checked,
-         hasUpper = upperCaseEl.checked,
-         hasNumber = numbersEl.checked,
-         hasSymbol = symbolsEl.checked;
-
-     resultEl.textContent = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
- })
+ toggles.forEach(toggle => toggle.addEventListener("change", doTheTrick));
